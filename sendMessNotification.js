3df1c521b7ed248978d7d1286ebf3dfa7fec0c45
POTHURAJU VYSHRAWAN <121814561+VyshrawanP@users.noreply.github.com>
2026-01-25 14:28:26 +0530

    Fix typo in JSON file name from 'mess_menu' to 'mess_menue'

--- sendMessNotification.js
+++ sendMessNotification.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 
 const meal = process.argv[2];
 
-const data = JSON.parse(fs.readFileSync("mess_menu.json", "utf-8"));
+const data = JSON.parse(fs.readFileSync("mess_menue.json", "utf-8"));
 
 const today = new Date();
 const todayDate = today.getDate();
