84e32449a02e329d52fb3668ac9efe3c8fd8b4bc
VYSHRAWAN P <vyshrawanp@VYSHRAWANs-MacBook-Air.local>
2026-01-26 22:13:26 +0530

    fixed multiple query executions things into single

--- api/send-manual.js
+++ api/send-manual.js
@@ -18,19 +18,15 @@ export default async function handler(req, res) {
   if (!title || !message) {
     return res.status(400).json({ error: "title and message required" });
   }
+await admin.messaging().send({
+  topic: "mess-alerts",
+  data: {
+    title: titles[meal],
+    body,
+    link: "https://vcampusgo.vercel.app"
+  }
+});
 
-  await admin.messaging().send({
-    topic: "mess-alerts",
-    notification: {
-      title,
-      body: message
-    },
-    webpush: {
-      fcmOptions: {
-        link: link || "https://vcampusgo.vercel.app"
-      }
-    }
-  });
 
   res.json({ sent: true });
 }
