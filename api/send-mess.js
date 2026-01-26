1a2e872c92616cc950518f9a47259fd65426a885
VYSHRAWAN P <vyshrawanp@VYSHRAWANs-MacBook-Air.local>
2026-01-26 15:29:16 +0530

    Maximum one notification per meal

--- api/send-mess.js
+++ api/send-mess.js
@@ -10,12 +10,23 @@ if (!admin.apps.length) {
   });
 }
 
+const db = admin.firestore();
+
 export default async function handler(req, res) {
   const { meal } = req.query; // breakfast | lunch | snacks | dinner
   const istDate = new Date(
   new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
 );
 const today = istDate.getDate().toString();
+
+  const docId = `${todayKey}_${meal}`;
+  const docRef = db.collection("daily_notifications").doc(docId);
+  const docSnap = await docRef.get();
+
+  if (docSnap.exists) {
+    console.log("⛔ Notification already sent:", docId);
+    return res.json({ skipped: true, reason: "already_sent_today" });
+  }
 
 
   const menuPath = path.join(process.cwd(), "public/data/mess_menue.json");
@@ -48,5 +59,18 @@ const today = istDate.getDate().toString();
     }
   });
 
+
+
+await docRef.set({
+    meal,
+    date: todayKey,
+    sentAt: admin.firestore.FieldValue.serverTimestamp()
+  });
+
+  console.log("✅ Notification sent:", docId);
+
+  return res.json({ sent: true, meal });
+
   res.json({ sent: meal });
 }
+
